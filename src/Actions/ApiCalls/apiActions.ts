b555150f425621b4130eab0e58438d99a7de2766
SammieMenz <sammieackun@hotmail.com>
2025-03-07 00:33:33 +0000

    first commit

--- src/Actions/ApiCalls/apiActions.ts
+++ src/Actions/ApiCalls/apiActions.ts
@@ -1,5 +1,5 @@
-//const BASE_URL = "https://rimcs-wsproject.vercel.app";
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://rimcs-wsproject.vercel.app";
+//const BASE_URL = "http://localhost:3000";
 
 export const getAPIDATA = (tableName: string) => {
   return `${BASE_URL}/api/${tableName.toLowerCase()}`;
